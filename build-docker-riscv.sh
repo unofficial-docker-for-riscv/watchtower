090a7e7d43af570cd7ef5dec762ac6ac0a5d846c
ngc7331 <ngc7331@outlook.com>
2024-11-26 19:03:04 +0800

    chore(ci): add docker-build-riscv.sh

---
+++ build-docker-riscv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# DO NOT UPSTREAM THIS FILE
+
+VERSION=$(curl -s https://api.github.com/repos/containrrr/watchtower/releases/latest | jq -r .tag_name)
+
+REPOS=(${REPOS:-ngc7331/watchtower})
+TAGS=()
+for repo in ${REPOS[@]}; do
+	TAGS+=("-t ${repo,,}:${VERSION}")
+  TAGS+=("-t ${repo,,}:latest")
+done
+
+docker buildx build \
+  ${TAGS[@]} \
+  --push \
+  --build-arg WATCHTOWER_VERSION=${VERSION} \
+  --platform linux/riscv64 \
+  -f dockerfiles/Dockerfile.self-contained \
+  .
