9c0f93138779320f1b7cb4523a67b6ae593cc015
ngc7331 <ngc7331@outlook.com>
2024-12-01 00:22:23 +0800

    build: add riscv- prefix to dockerhub repo

--- build-docker-riscv.sh
+++ build-docker-riscv.sh
@@ -3,7 +3,7 @@
 
 VERSION=$(curl -s https://api.github.com/repos/containrrr/watchtower/releases/latest | jq -r .tag_name)
 
-REPOS=(${REPOS:-ngc7331/watchtower})
+REPOS=(${REPOS:-ngc7331/riscv-watchtower})
 TAGS=()
 for repo in ${REPOS[@]}; do
 	TAGS+=("-t ${repo,,}:${VERSION}")
